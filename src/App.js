981fb23dd37395ab7dedb6a88e39d2602d695329
Keegan Wolf <kwolf313@live.com>
2024-06-18 22:17:39 -0500

    Tailwind and Daisy UI config.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import './output.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+
+		<>
+
+			<button class="btn btn-primary">
+				<span className="loading loading-spinner"></span>
+				Button
+			</button>
+
+			{/* Open the modal using document.getElementById('ID').showModal() method */}
+			<button className="btn" onClick={()=>document.getElementById('my_modal_2').showModal()}>open modal</button>
+			<dialog id="my_modal_2" className="modal">
+			<div className="modal-box">
+				<h3 className="font-bold text-lg">Hello!</h3>
+				<p className="py-4">Press ESC key or click outside to close</p>
+			</div>
+			<form method="dialog" className="modal-backdrop">
+				<button>close</button>
+			</form>
+			</dialog>
+
+		</>
+
+	);
 }
-
-export default App;
